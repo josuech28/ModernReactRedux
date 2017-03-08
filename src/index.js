b525de5382c52c4b4996a13398d59463245db14f
Josue Chinchilla <josue.chinchilla@prodigious.com>
2017-03-08 13:28:03 -0600

    Modern React Redux chapter 23

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCmcJ84aeXJmiKtzdUNIgkFd7kWlyqf6Sk';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-	console.log(data);
-});
+
 // Create a new component. This component should produce some html
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			videos: []
+		};	
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });	
+		});	
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList  videos={this.state.videos} />
+			</div>
+		);
+	}
+	
 }
 
 
